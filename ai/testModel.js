cd3a2951f55fe14cd49e2d42634eabf29863ce95
nhutlong-nguyen <nhutlongnguyen.work@gmail.com>
2023-11-11 16:06:58 +0200

    feat: add ai

---
+++ ai/testModel.js
@@ -0,0 +1,36 @@
+const tf = require('@tensorflow/tfjs-node');
+const fs = require('fs');
+const trainingDataPath = './training_data.json';
+const trainingData = JSON.parse(fs.readFileSync(trainingDataPath, 'utf8'));
+
+function getActivityIndex(activity) {
+    const activityValues = { 'swim': 0, 'run': 1, 'bike': 2 };
+    return activityValues[activity] || -1;
+}
+
+function oneHotEncode(index, numClasses) {
+    return Array.from({ length: numClasses }, (_, i) => i === index ? 1 : 0);
+}
+
+// Convert training data to tensors
+const inputTensor = tf.tensor2d(trainingData.map(data => [parseFloat(data.dailyCaloriesBurnGoal), getActivityIndex(data.preferActivity)]));
+const labelTensor = tf.tensor2d(trainingData.map(data => oneHotEncode(getActivityIndex(data.preferActivity), 3)), 'float32');
+
+// Create a sequential model
+const model = tf.sequential();
+model.add(tf.layers.dense({ inputShape: [2], units: 10, activation: 'relu' }));
+model.add(tf.layers.dense({ units: 3, activation: 'softmax' })); // 3 units for swim, run, and bike
+
+// Compile the model
+model.compile({
+  optimizer: tf.train.adam(),
+  loss: 'categoricalCrossentropy',
+  metrics: ['accuracy'],
+});
+
+// Train the model
+model.fit(inputTensor, labelTensor, { epochs: 100 }).then(() => {
+  model.save('file://./activity_model').then(() => {
+    console.log('Model saved.');
+  });
+});
