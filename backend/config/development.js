465e09f054966a47cd671d9c328e0991d003169b
thuy0605 <thanhthuyta1989@gmail.com>
2024-01-11 10:41:44 +0200

    FitRank project added

--- backend/config/development.js
+++ backend/config/development.js
@@ -1,10 +1,12 @@
-'use strict';
-require('dotenv').config();
+"use strict";
+require("dotenv").config();
 
 module.exports = {
-  env: 'development',
-  db: process.env.MONGODB_URI,
-  port: process.env.PORT || 4000,
-  jwtIssuer: 'localhost:4000',
-  jwtExpires: 86400
+  env: "development",
+  db:
+    process.env.MONGODB_URI ??
+    "mongodb+srv://tuktuk:123!456@cluster1.7ohob.mongodb.net/tuktuk?retryWrites=true&w=majority",
+  port: process.env.PORT ?? 4000,
+  jwtIssuer: "localhost:4000",
+  jwtExpires: 86400,
 };
